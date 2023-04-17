67d9545719dfb2492d4e392bd26d91e37342e042
Avinash Biradar <biradarav.100@gmail.com>
2023-04-17 23:27:30 +0530

    Json data to excel file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {useState,useEffect} from 'react';
-import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
   
 function App(){
  
   const[result,setResult]= useState([]);
 
   const getData = ()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
     .then(res => setResult( res));
   }
@@ -15,42 +16,23 @@ function App(){
       getData();
   },[])
 
-  return (
-    <div className="container">
-        <h3 className="mt-3 text-success"><center>Export React Table Data into EXCEL Sheet</center></h3>
-        <div className="row mt-4">
-        <ReactHTMLTableToExcel
-            id="test-table-xls-button"
-            className="download-table-xls-button btn btn-success mb-3"
-            table="table-to-xls"
-            filename="tablexls"
-            sheet="tablexls"
-            buttonText="Export Data to Excel Sheet"/>
-            <table className="table" id="table-to-xls">
-            <thead className="thead-dark">
-            <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Email</th>
-                <th>Website</th>
-            </tr>
-            </thead>
-            <tbody>
-            
-                  {result.map((res)=>
-                    <tr>
-                    <td>{res.name}</td>
-                    <td>{res.username}</td>
-                    <td>{res.email}</td>
-                    <td>{res.website}</td>
-                    </tr>
-                  )}   
-                
-            </tbody>   
-        </table>
-      </div>
-    </div>
-  );
+  const handleExport = () => {
+
+    // Create an Excel workbook
+    const workbook = XLSX.utils.book_new();
+
+    // Convert JSON data to a worksheet
+    const worksheet = XLSX.utils.json_to_sheet(result);
+
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Data");
+
+    // Export the workbook to an Excel file
+    const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+    saveAs(new Blob([excelBuffer], { type: "application/octet-stream" }), "data.xlsx");
+  };
+
+  return <button onClick={handleExport}>Export to Excel</button>;
 }
   
-export default App
+export default App;
